c5b968d97b46daf08cd1059d5c8bd02815f59884
Matt Johnston <matt.tw.johnston@gmail.com>
2018-08-26 09:00:07 -0300

    feat(entirety): implemented simple library for error management
    made it easy to create, log, and show errors by creating a new class

--- src/easy-e.ts
+++ src/easy-e.ts
@@ -1,26 +1,41 @@
-interface Alerter {
+export interface Alerter {
   (messageForHumans: string): void
 }
 
 export default class Erric {
   public code: string
   public messageForHumans: string
-  public meta: object
+  public meta?: object
   public alerter: Alerter
 
-  constructor(code: string, messageForHumans: string, meta: object) {
+  constructor(
+    code: string = 'erric/error-construction/no-error-code-provided',
+    messageForHumans: string = 'no message for humans set',
+    meta?: object
+  ) {
     this.code = code
     this.messageForHumans = messageForHumans
-    this.meta = meta
-    this.alerter = messageForHumans => {
+    if (meta) {
+      this.meta = meta
+    }
+
+    this.alerter = msg => {
       console.log(
         'call setAlerter(to assign a function that takes a message, and shows it to users'
       )
     }
   }
 
+  public setAlerter(alerter: Alerter) {
+    this.alerter = alerter
+  }
+
   public alert() {
     this.alerter(this.messageForHumans)
+  }
+
+  public loud() {
+    console.error(this)
   }
 
   public setCode(str: string) {
--- test/easy-e.test.ts
+++ test/easy-e.test.ts
@@ -1,14 +1,64 @@
-import DummyClass from '../src/easy-e'
+import Erric from '../src/easy-e'
+import { Alerter } from '../src/easy-e'
+const any = expect.any
 
-/**
- * Dummy test
- */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+describe('Erric', () => {
+  describe('should be chill with no args in the constructor', () => {
+    it('should exist after constructing with no params', () => {
+      const defaultErric = new Erric()
+      expect(defaultErric).toBeDefined()
+    })
+    it(`should have a public method 'alert'`, () => {
+      const defaultErric = new Erric()
+      expect(defaultErric.alert).toBeInstanceOf(Function)
+    })
+    it(`should have an 'alerter' by default`, () => {
+      const defaultErric = new Erric()
+      expect(defaultErric.alerter).toBeDefined()
+    })
+    it(`should have an 'alerter' by default`, () => {
+      const defaultErric = new Erric()
+      expect(defaultErric.alerter).toBeInstanceOf(Function)
+    })
+    it(`should have a public property called 'code' by default`, () => {
+      const defaultErric = new Erric()
+      expect(defaultErric.code).toEqual(expect.stringContaining('code'))
+    })
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  describe(`should invoke alert with defined msg for human`, () => {
+    it(`should invoke alert with public property 'messageForHuman'`, () => {
+      const defaultErric = new Erric('error-code/null', 'beep boop')
+      const alerter = function(msg) {
+        console.log(msg)
+      }
+      defaultErric.setAlerter(alerter)
+      expect(defaultErric.alerter).toEqual(alerter)
+    })
+
+    describe(`should have setters that set`, () => {
+      it(`should set code when 'setCode' is called`, () => {
+        const erric = new Erric('error/everything/failing/perfectly')
+      })
+    })
+
+    describe(`should set properties with constructor params`, () => {
+      it(`should set code to arg0`, () => {
+        const erric = new Erric('error/jk')
+        expect(erric.code).toEqual('error/jk')
+      })
+
+      it(`should set message for humans to arg1`, () => {
+        const humanReadableMessage = 'there is an error, NOT'
+        const erric = new Erric('error/jk/again', humanReadableMessage)
+        expect(erric.messageForHumans).toEqual(humanReadableMessage)
+      })
+
+      it(`should set meta arg2`, () => {
+        const arbitraryData = { input: 'arbitrary data' }
+        const erric = new Erric('error/yea-right', 'preventing meta bugs', arbitraryData)
+        expect(erric.meta).toEqual(arbitraryData)
+      })
+    })
   })
 })
